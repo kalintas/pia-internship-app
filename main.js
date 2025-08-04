d516575c003cf2c57dd9979b7cfc3e0163a3eef2
kalintas <keremkalinntas@gmail.com>
2025-08-04 16:02:48 +0300

    Add popups

--- main.js
+++ main.js
@@ -1,4 +1,30 @@
 
+function createProductDiv(product) {
+    let createDiv = (value, id) => {
+        let div = document.createElement("div");
+        let divText = document.createTextNode(value);
+        div.appendChild(divText);
+        div.className = "product" + id.charAt(0).toUpperCase() + id.slice(1);
+        div.id = id; 
+        return div;
+    };
+    
+    let div = document.createElement("div");
+    div.className = "product";
+
+    let image = document.createElement("img");
+    image.src = product.imageurl;
+    image.className = "productImage";
+    div.appendChild(image);
+
+    div.appendChild(createDiv(product.name, "name"));
+    div.appendChild(createDiv(product.category, "category"));
+    div.appendChild(createDiv(product.description, "description"));
+    div.appendChild(createDiv(product.price, "price"));
+
+    return div;
+}
+
 function search() {
 
     let name = document.getElementById('nameInput').value;
@@ -13,26 +39,20 @@ function search() {
     table.innerHTML = "";
    
     searchResult.forEach((product) => {
-        let createDiv = (value, id) => {
-            let div = document.createElement("div");
-            let divText = document.createTextNode(value);
-            div.appendChild(divText);
-            div.id = id; 
-            return div;
-        };
-        
-        let div = document.createElement("div");
-        div.className = "product";
+        let div = createProductDiv(product);
 
-        let image = document.createElement("img");
-        image.src = product.imageurl;
-        image.className = "productImage";
-        div.appendChild(image);
+        div.onclick = (event) => {
+            let popupBackground = document.getElementById('popupBackground');
+            popupBackground.innerHTML = "";
+            popupBackground.style.pointerEvents = "all";
 
-        div.appendChild(createDiv(product.name, "name"));
-        div.appendChild(createDiv(product.category, "category"));
-        div.appendChild(createDiv(product.description, "description"));
-        div.appendChild(createDiv(product.price, "price"));
+            let productDiv = createProductDiv(product);
+            productDiv.id = "popupBackgroundProduct";
+            productDiv.className = "productBig";
+           
+            popupBackground.appendChild(productDiv);
+            event.stopPropagation();
+        }
 
         table.appendChild(div);
     });    
@@ -40,4 +60,15 @@ function search() {
 
 document.addEventListener("DOMContentLoaded", function(e) {
     search();
+});
+
+window.addEventListener('click', (event) => {   
+    let product = document.getElementById('popupBackgroundProduct');
+    if (!product) {
+        return;
+    }
+    if (!product.contains(event.target)) {
+        popupBackground.innerHTML = "";
+        popupBackground.style.pointerEvents = "none";
+    }
 });
